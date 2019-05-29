563d7a29830b9539f8b5de79d7c69229a7c79f82
marissaverdonck <marissa.verdonck@hva.nl>
2019-05-29 19:34:03 +0200

    update

--- server.js
+++ server.js
@@ -4,40 +4,25 @@ const camelCase = require('camel-case');
 const express = require('express');
 const app = express();
 const port = 3000;
-//makes a string (such as a title) URL safe.
+//Makes a string (such as a title) URL safe.
 const slug = require('slug');
-//ontleed things as json, forms, etc.
+//Ontleed things as json, forms, etc.
 const bodyParser = require('body-parser');
-//uploading files in forms
+//Uploading files in forms
 const multer = require('multer');
 var find = require('array-find')
 var mongo = require('mongodb')
 require('dotenv').config()
-
+  //Folder dor uploaded files
+var upload = multer({ dest: 'static/upload/' })
+  // Mongodb
 var db = null
 var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
-
 mongo.MongoClient.connect(url, function(err, client) {
-  if (err) throw err
+  if (err)
+    throw err
   db = client.db(process.env.DB_NAME)
 })
-
-//folder dor uploaded files
-var upload = multer({ dest: 'static/upload/' })
-
-var data = [{
-    id: 'marissa',
-    email: 'Marissa',
-    password: 27,
-
-  },
-  {
-    id: 'danny',
-    email: 'Danny',
-    password: 21,
-
-  }
-];
 
 //Serve files from the static folder (middleware function)
 app.use(express.static('static'));
@@ -98,39 +83,22 @@ app.get('/user1', function(req, res) {
 app.get('/itsamatch', function(req, res) {
   res.render('itsamatch')
 });
-// ga naar profielpagina
-app.get('/:id', user);
-app.delete('/:id', remove);
+
+
+
+
 
 //Handle a post request to /
 app.post('/createaccount1', add1);
 app.post('/createaccount2', upload.single('profilepicture'), add2)
-
-// If no valid URL was found, send the "not-found page"
+  // Gives the portnumber
 app.use(function(req, res) {
   res.status(404).render('not-found')
 });
-// Gives the portnumber
-app.listen(port, function() {
-  console.log('The app listening on port ${port}!')
-});
+// ga naar profielpagina
+app.get('/:id', user);
+app.delete('/:id', remove);
 
-function add(req, res, next) {
-  db.collection('data').insertOne({
-    title: req.body.title,
-    cover: req.file ? req.file.filename : null,
-    plot: req.body.plot,
-    description: req.body.description
-  }, done)
-
-  function done(err, data) {
-    if (err) {
-      next(err)
-    } else {
-      res.redirect('/' + data.insertedId)
-    }
-  }
-}
 
 // Haal de gegevens uit de data van /:id en open het in de pagina (profile)
 function user(req, res, next) {
@@ -148,22 +116,6 @@ function user(req, res, next) {
   }
 }
 
-// delete data
-function remove(req, res) {
-  var id = req.params.id
-  db.collection('data').deleteOne({
-    _id: new mongo.ObjectID(id)
-  }, done)
-
-  function done(err) {
-    if (err) {
-      next(err)
-    } else {
-      res.json({ status: 'ok' })
-    }
-  }
-}
-
 function add1(req, res) {
   var id = slug(req.body.email).toLowerCase();
 
@@ -172,9 +124,6 @@ function add1(req, res) {
     email: req.body.email,
     password: req.body.password
   }, done)
-
-  console.log(data);
-  console.log(id);
 
   function done(err, data) {
     if (err) {
@@ -200,8 +149,6 @@ function add2(req, res) {
     agetill: req.body.agetill,
     profilepicture: req.file ? req.file.filename : null,
   }, done)
-  console.log(data);
-  console.log(id);
 
   function done(err, data) {
     if (err) {
@@ -213,6 +160,27 @@ function add2(req, res) {
   }
 }
 
+// delete data
+function remove(req, res) {
+  var id = req.params.id
+  db.collection('data').deleteOne({
+    _id: new mongo.ObjectID(id)
+  }, done)
+
+  function done(err) {
+    if (err) {
+      next(err)
+    } else {
+      res.json({ status: 'ok' })
+    }
+  }
+}
+
+// If no valid URL was found, send the "not-found page"
+
+app.listen(port, function() {
+  console.log('The app listening on port ${port}!')
+});
 /* Bronnen:
 dandevri, 2019- mongodb-server - https://github.com/cmda-bt/be-course-18-19/blob/master/examples/mongodb-server/index.js
 dandevri, 2019- Express-server - https://github.com/cmda-bt/be-course-18-19/blob/master/examples/express-server/index.js
