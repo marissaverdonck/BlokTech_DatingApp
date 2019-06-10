4cabf3e63483733e319f6c724d35ece50b1d88db
Marissa Verdonck <marissa@MacBook-Pro-van-Marissa.local>
2019-06-10 16:12:58 +0200

    update

--- functions/checkLogin.js
+++ functions/checkLogin.js
@@ -1,13 +1,14 @@
 // Require dependencies
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const multer = require('multer');
 const mongo = require('mongodb');
-const session = require('express-session');
 var upload = multer({ dest: 'static/upload/' });
-var db = null;
 require('dotenv').config();
-var url = process.env.DB_HOST
-mongo.MongoClient.connect(url, function(err, client) {
+var url = process.env.DB_HOST;
+mongo.MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
   if (err)
     throw err
   db = client.db(process.env.DB_NAME)
